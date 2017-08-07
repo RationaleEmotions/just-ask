df07ae20f61fd5812ff3e879663f7b36e222e139
Philippe GRANET <philippe.granet@gmail.com>
2017-08-08 01:14:57 +0200

    Can use default docker unix socket

--- src/main/java/com/rationaleemotions/server/DockerBasedSeleniumServer.java
+++ src/main/java/com/rationaleemotions/server/DockerBasedSeleniumServer.java
@@ -2,10 +2,12 @@ package com.rationaleemotions.server;
 
 import com.rationaleemotions.config.ConfigReader;
 import com.rationaleemotions.server.docker.DeviceInfo;
+import com.spotify.docker.client.UnixConnectionSocketFactory;
 import com.spotify.docker.client.exceptions.DockerException;
 import org.openqa.grid.internal.TestSession;
 import org.openqa.selenium.remote.CapabilityType;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,9 +37,14 @@ public class DockerBasedSeleniumServer implements ISeleniumServer {
     }
 
     @Override
-    public String getHost() {
-        return ConfigReader.getInstance().getDockerHost();
-    }
+	public String getHost() {
+    	URI uri=ConfigReader.getInstance().getDockerRestApiUri();
+		if (uri.getScheme().equals(DockerHelper.UNIX_SCHEME)) {
+			return UnixConnectionSocketFactory.sanitizeUri(uri).getHost();
+		} else {
+			return uri.getHost();
+		}
+	}
 
     @Override
     public void shutdownServer() throws ServerException {
